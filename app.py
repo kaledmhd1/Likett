dcc70e427bbc4359e189cfb241fd6cbef9a2a53f
kaledmhd1 <Isameryem211@gmail.com>
2025-07-28 03:18:06 +0100

    app.py

--- app.py
+++ app.py
@@ -152,5 +152,7 @@ def send_friend_requests(sv_number=None):
 
     return jsonify({"message": "done", "results": results})
 
+import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
